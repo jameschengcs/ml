aa535d1aaa3450fb8ddf82f8ebee8ab913a27e7d
jameschengcs <chengchcWcs.nctu.edu.tw>
2018-04-22 14:08:18 +0800

    Naive Bayes in sklearn

---
+++ NaiveBayes.py
@@ -0,0 +1,17 @@
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+
+iris = datasets.load_iris()
+n_samples = int(len(iris) * 0.8)
+trainData = iris.data[:n_samples]
+testData = iris.data[n_samples:]
+trainTarget = iris.target[:n_samples]
+testTarget = iris.target[n_samples:]
+
+gnb = GaussianNB()
+gnb.fit(trainData, trainTarget)
+predictTarget = gnb.predict(testData)
+
+print("Number of mislabeled points out of a total %d points : %d"
+      %(iris.data.shape[0],(testTarget != predictTarget).sum()))
+
