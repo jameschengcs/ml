982ccb82237db7ea0fb4a95bf55b0dbf22bbba99
jameschengcs <chengchcWcs.nctu.edu.tw>
2018-04-22 11:17:01 +0800

    K-means example

--- kmeans.py
+++ kmeans.py
@@ -3,6 +3,7 @@ import numpy as np
 
 X = np.array([[0.5, 2], [1, 4.5], [1, 0.25],
              [4, 2], [4, 4], [4, 0]])
+
 kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 print(kmeans.labels_)
 print(kmeans.cluster_centers_)
