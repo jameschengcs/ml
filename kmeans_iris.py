9fe897309e12946e43ace5ddaeb4d2f0c5fdbef6
jameschengcs <chengchcWcs.nctu.edu.tw>
2018-04-24 10:01:58 +0800

    K Means with iris

---
+++ kmeans_iris.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numpy import linalg as LA
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+
+iris = load_iris()
+for nK in range(1, 6):
+    kmeans = KMeans(n_clusters=nK, random_state=0).fit(iris.data)
+    
+    sumd = np.ndarray(shape=(nK, 1))
+    
+    for i in range(len(iris.data)):
+        k = kmeans.labels_[i]
+        ui = kmeans.cluster_centers_[k]
+        d = LA.norm(iris.data[i] - ui)
+        sumd[k] += d * d
+    
+    ak = np.sum(sumd) / nK     
+    print("k = ", nK, ", ak = ", ak)
+    
+
+
