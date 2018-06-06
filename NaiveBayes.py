9c5457b04c14fc8ad85b3aad35974629e58b0c7f
jameschengcs <chengchc@cs.nctu.edu.tw>
2018-06-06 13:16:08 +0800

    Naive Bayes in sklearn

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 from sklearn.naive_bayes import GaussianNB
 
 iris = datasets.load_iris()
+print(len(iris.data))
 n_samples = int(len(iris) * 0.8)
 trainData = iris.data[:n_samples]
 testData = iris.data[n_samples:]
