3eb0338c4290b652cc9fa8bd459abfc4e62e5d93
jameschengcs <chengchc@cs.nctu.edu.tw>
2019-05-22 12:01:32 +0800

    Update cnn_mnist.py

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -53,13 +53,13 @@ x_image = tf.reshape(xs, [-1, 28, 28, 1])
 print(x_image.shape)  # [n_samples, 28,28,1]
 
 ## conv1 layer ##
-W_conv1 = weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32
+W_conv1 = weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32 (32 filters with size of 5 * 5 for 1 input)
 b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
 h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
 
 ## conv2 layer ##
-W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
+W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64 (64 filters with size of 5 * 5 for 32 input)
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
 h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
