992b5aee9ec606b5e7302142c447ed6ee7679bcf
jameschengcs <chengchc@cs.nctu.edu.tw>
2019-07-29 15:10:11 +0800

    Update tf_dataset1.py

--- tf_dataset1.py
+++ tf_dataset1.py
@@ -7,15 +7,15 @@ print('from_tensor_slices')
 ds = tf.data.Dataset.from_tensor_slices((X, Y))
 print(ds)
 itr = ds.make_one_shot_iterator()
-item = itr.get_next()
+elem = itr.get_next()
 with tf.Session() as sess:
     for i in range(7):
-        print(sess.run(item))
+        print(sess.run(elem))
         
 print('from_tensors')
 ds = tf.data.Dataset.from_tensors((X, Y))
 print(ds)
 itr = ds.make_one_shot_iterator()
-item = itr.get_next()
+elem = itr.get_next()
 with tf.Session() as sess:
-    print(sess.run(item))
+    print(sess.run(elem))
