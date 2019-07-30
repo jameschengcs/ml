be832cb5c277cef11fb00c5797ca676792fd7fea
jameschengcs <chengchc@cs.nctu.edu.tw>
2019-07-30 14:44:55 +0800

    Update tf_dataset3.py

--- tf_dataset3.py
+++ tf_dataset3.py
@@ -5,6 +5,6 @@ tfe.enable_eager_execution() # it must be called at program startup.
 X = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14]] # (7, 2)
 Y = [12, 34, 56, 78, 910, 1112, 1314]
 
-ds = tf.data.Dataset.from_tensor_slices((X, Y)).shuffle(buffer_size = 1000).repeat(3).batch(3)
+ds = tf.data.Dataset.from_tensor_slices((X, Y)).shuffle(buffer_size = 7).repeat(3).batch(3)
 for elem in tfe.Iterator(ds):
     print(elem)  
